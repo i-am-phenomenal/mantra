d2644706000cb6623063eb5a4b3993e1826be450
Aditya Chaturvedi <chat29aditya@gmail.com>
2021-03-07 15:05:03 +0530

    WIP: need to add await on GET for book

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/styles';
+import React from "react";
+import axios from 'axios'; 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect 
+} from "react-router-dom";
+import BooksList from "./Components/List";
+import BookDialog from "./Components/BookDialog";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      baseUrl:  "http://18.179.108.80:8080/",
+      books: [],
+      isBookModalOpen: false,
+      currentBookId: -1
+    }
+  }
+
+  updateStateforBooks = (books) => {
+    this.setState({books: books});
+  }
+
+  getBooks = () => {
+    axios.get(this.state.baseUrl + "books/")
+    .then((response) => {this.updateStateforBooks(response.data)})
+    .catch((error) => alert(error))
+  }
+
+  openBookModal = (event, book) => {
+    event.preventDefault();
+    this.setState({isBookModalOpen: true, currentBookId: book.id});
+  }
+
+  componentDidMount() {
+    {this.getBooks()}
+  }
+
+  updateDialogState = (state) => {
+    this.setState({isBookModalOpen: state})
+  }
+
+  renderFullScreenDialog = () => {
+    if (this.state.isBookModalOpen) {
+      return <BookDialog  open = {true} setOpen = {this.updateDialogState} bookId = {this.state.currentBookId} />
+    } else {
+      return ""
+    }
+  }
+
+  render() {
+    return (
+      <div> 
+        {this.renderFullScreenDialog()}
+        <BooksList books = {this.state.books} onClickHandler= {this.openBookModal} /> 
+      </div>
+      ) 
+  }
+
 }
-
 export default App;
---
+++ src/Components/List.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: '36ch',
+    backgroundColor: theme.palette.background.paper,
+  },
+  inline: {
+    display: 'inline',
+  },
+}));
+
+export default function BooksList(props) {
+  const classes = useStyles();
+  return (
+    <List className={classes.root}>
+        {props.books.map(book => {
+      return (
+                <ListItem alignItems="flex-start">
+                <Button color="primary" onClick={(event) => props.onClickHandler(event, book)}>{book.title}</Button>
+            </ListItem>
+            )
+        })}
+    </List> 
+  );
+}
